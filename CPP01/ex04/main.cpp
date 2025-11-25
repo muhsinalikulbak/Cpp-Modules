4113d2e0b892a33af42cb4642688e9c78cee3f24
muhsinalikulbak <muhsin.kulbak@gmail.com>
2025-11-25 18:34:25 +0300

    .gitignore eklendi, zombiHorde fonksiyonu zombieHorde.cpp dosyasına taşındı

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mkulbak <mkulbak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/23 16:35:53 by mkulbak           #+#    #+#             */
-/*   Updated: 2025/11/23 19:44:37 by mkulbak          ###   ########.fr       */
+/*   Updated: 2025/11/25 18:21:38 by mkulbak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,9 @@
 
 int main(int argc, char const *argv[])
 {
-    
+    if (argc != 4)
+    {
+        std::cout << "Invalid argument" << std::endl;
+        return 1;
+    }
 }
