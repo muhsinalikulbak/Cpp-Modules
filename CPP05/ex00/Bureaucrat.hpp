29b12b61457ad59850c95803ddd417f003e8fd37
Muhsin <muhsin.kulbak@gmail.com>
2026-02-25 00:00:25 +0300

    Exception sınıfları yazıldı

--- CPP05/ex00/Bureaucrat.hpp
+++ CPP05/ex00/Bureaucrat.hpp
@@ -10,6 +10,7 @@ private:
     const std::string _name;
     int _grade;
 public:
+
     Bureaucrat();
     ~Bureaucrat();
     Bureaucrat(const Bureaucrat& other);
@@ -20,8 +21,23 @@ public:
     void incrementGrade();
     void decrementGrade();
 
-    
+    class GradeTooHighException : public std::exception
+    {
+        public:
+            virtual const char* what() const throw() {
+                return "The bureaucrat's score is very high (cannot be less than 1)!";
+            }
+    };
+
+    class GradeTooLowException
+    {
+        public:
+            virtual const char* what() const throw(){
+                return "The bureaucrat's score is too low (cannot be greater than 150)!";
+            }
+    };
 };
+
 std::ostream & operator<<(std::ostream & os, const Bureaucrat& rhs);
 
 
