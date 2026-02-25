8038e799edf185a4fa8206f7e29429ce65889d31
Muhsin <muhsin.kulbak@gmail.com>
2026-02-25 06:17:35 +0300

    Increment ve decreemnt fonksiyonlarına throw exception eklendi, << ostream overload yazıldı

--- CPP05/ex00/Bureaucrat.cpp
+++ CPP05/ex00/Bureaucrat.cpp
@@ -2,50 +2,56 @@
 
 Bureaucrat::Bureaucrat() : _name("")
 {
-    std::cout << "Called default constructor" << std::endl;
-    _grade = 0; 
+	std::cout << "Called default constructor" << std::endl;
+	_grade = 0;
 }
 
 Bureaucrat::~Bureaucrat()
 {
-    std::cout << "Destructor" << std::endl;
+	std::cout << "Destructor" << std::endl;
 }
 
 Bureaucrat::Bureaucrat(const Bureaucrat& other) : _name(other._name), _grade(other._grade)
 {
-    std::cout << "Called copy constructor" << std::endl;
+	std::cout << "Called copy constructor" << std::endl;
 }
 
 Bureaucrat&  Bureaucrat::operator = (Bureaucrat& rhs)
 {
-    std::cout << "Called copy assignment operator" << std::endl;
-    if (this != &rhs)
-    {
-        _grade = rhs._grade;
-    }
+	std::cout << "Called copy assignment operator" << std::endl;
+	if (this != &rhs)
+	{
+		_grade = rhs._grade;
+	}
+	return *this;
 }
-
 
 std::string Bureaucrat::getName() const
 {
-    return _name;
+	return _name;
 }
 
 int Bureaucrat::getGrade() const
 {
-    return _grade;
+	return _grade;
 }
 
 void Bureaucrat::incrementGrade()
 {
-    _grade--;
+	if (_grade - 1 < 1)
+		throw Bureaucrat::GradeTooHighException();
+	_grade--;
 }
+
 void Bureaucrat::decrementGrade()
 {
-    _grade++;
+	if (_grade + 1 > 150)
+		throw Bureaucrat::GradeTooLowException();
+	_grade++;
 }
 
 std::ostream & operator<<(std::ostream & os, const Bureaucrat& rhs)
 {
-    
+	os << rhs.getName() << ", bureaucrat grade " << rhs.getGrade();
+	return os;
 }
