0927e1e5bde094cf2b8be4e5f699ff0320e25f14
Muhsin <muhsin.kulbak@gmail.com>
2026-04-13 16:16:00 +0300

    CPP09 dosyaları oluşturuldu

---
+++ CPP09/BitcoinExchange.hpp
@@ -0,0 +1,19 @@
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANHE_HPP
+
+
+
+class BitcoinExchange
+{
+private:
+    /* data */
+public:
+    BitcoinExchange(/* args */);
+    ~BitcoinExchange();
+};
+
+
+
+
+
+#endif
